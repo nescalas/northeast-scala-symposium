cee0d8086e81c5dded465280e2f22254ffa44758
Brian Clapper <bmc@clapper.org>
2015-12-07 10:31:03 -0500

    Disabled stub test, as it gets in the way of sbt-assembly.

--- src/test/scala/ExampleSpec.scala
+++ src/test/scala/ExampleSpec.scala
@@ -1,17 +1,17 @@
 package com.meetup
-
+/*
 import org.specs._
 
 import dispatch._
 
 object ExampleSpec extends Specification with unfiltered.spec.jetty.Served {
-  
+
   import dispatch._
-  
+
   def setup = { _.filter(new App) }
-  
+
   val http = new Http
-  
+
   "The example app" should {
     "serve unfiltered requests" in {
       val status = http x (host as_str) {
@@ -21,3 +21,4 @@ object ExampleSpec extends Specification with unfiltered.spec.jetty.Served {
     }
   }
 }
+*/
