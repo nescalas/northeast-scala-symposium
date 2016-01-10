abd63d92d625d8daf537e6846e0f3828c1632fa2
Brian Clapper <bmc@clapper.org>
2016-01-09 21:51:48 -0500

    Changes to config handling.

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -1,17 +1,23 @@
 package nescala
 
+import scala.collection.JavaConversions.propertiesAsScalaMap
+
 abstract class Config {
-  private lazy val props = {
+  private lazy val props: Option[Map[String,String]] = {
     Option(getClass.getResourceAsStream("/meetup.properties")).map { stream =>
       val props = new java.util.Properties
       props.load(stream)
       stream.close()
-      props
+      props.toMap
     }
   }
 
+  def propertyOption(name: String): Option[String] = {
+    Option(System.getenv(name)).orElse(props.flatMap(_.get(name)))
+  }
+
   def property(name: String) =
-    Option(System.getenv(name)).orElse(props.map(_.getProperty(name))) match {
+     propertyOption(name) match {
       case None => sys.error("missing property %s" format name)
       case Some(value) => value
     }
@@ -22,6 +28,16 @@ abstract class Config {
     } catch { case nfe: NumberFormatException =>
       sys.error("%s was not an int" format property(name))
     }
+
+  def booleanProperty(name: String, default: Boolean = false) =
+    propertyOption(name).map { s =>
+      s.toLowerCase match {
+        case "false" | "f" | "0" | "n" | "no" => false
+        case "true"  | "n" | "1" | "y" | "yes" => true
+        case _ => sys.error(s"""Bad boolean value "$s" for $name""")
+      }
+    }.
+    getOrElse(default)
 
   val siteSecret = property("secret")
 }
