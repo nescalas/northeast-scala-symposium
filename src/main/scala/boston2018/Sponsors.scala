f63f49240aa32d230e790904a18a707cd33ed652
Nick McAvoy <ohbadiah@outlook.com>
2018-03-05 17:05:53 -0600

    Minor copy change

--- src/main/scala/boston2018/Sponsors.scala
+++ src/main/scala/boston2018/Sponsors.scala
@@ -7,6 +7,6 @@ object Sponsor {
     Sponsor("CiBO Technologies", "http://www.cibotechnologies.com/", "/sponsors/cibo-technologies-logo.svg", "catering breakfast on Day 2"),
     Sponsor("Threat Stack", "http://threatstack.com/", "/sponsors/threat-stack-logo.png", "sponsoring lunch at [Clover Food Lab](https://www.cloverfoodlab.com/locations/location/?l=cloverknd) on the day of the Unconference."),
     Sponsor("Twilio", "http://twilio.com/", "/sponsors/twilio-logo.svg", "catering happy hour after the Unconference"),
-    Sponsor("Bridgewater Associates", "http://bwater.com/", "/sponsors/bridgewater-logo.svg", "sponsoring videography for us and for the Typelevel Summit.")
+    Sponsor("Bridgewater Associates", "http://bwater.com/", "/sponsors/bridgewater-logo.svg", "sponsoring videography for day 2 and for the Typelevel Summit.")
   )
 }
